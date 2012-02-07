a1310f34e35255eb81719f103e0cefbba86d688d
Mihhail Sidorin <uzzermail@gmail.com>
2012-02-07 14:28:54 +0200

    readme added
    Signed-off-by: Mihhail Sidorin <uzzermail@gmail.com>

--- qtdictapp.cpp
+++ qtdictapp.cpp
@@ -4,8 +4,59 @@
 
 qtDictApp::qtDictApp(QWidget *parent)
     : QWidget(parent)
-{
+{   
 
+    //Weclome screen
+    welcome_screen = new QVBoxLayout();
+    QLabel *welcome_text = new QLabel("QtDictApp");
+    QPushButton *connect_button = new QPushButton("Connect");
+    welcome_screen->addWidget(welcome_text);
+    welcome_screen->addWidget(connect_button);
+
+    //Processing
+    processing_screen = new QVBoxLayout();
+    QLabel *processing_text = new QLabel("processing...");
+    QPushButton *cancel_button = new QPushButton("Cancel");
+    processing_screen->addWidget(processing_text);
+    processing_screen->addWidget(cancel_button);
+
+    //Result screen
+    result_screen = new QVBoxLayout();
+    QLabel *result_text = new QLabel("this is result");
+    QPushButton *towelcome_button = new QPushButton("Back to welcome");
+    result_screen->addWidget(result_text);
+    result_screen->addWidget(towelcome_button);
+
+    QStateMachine machine;
+
+    QState *welcome_state = new QState(); machine.addState(welcome_state);
+    QState *processing_state = new QState(); machine.addState(processing_state);
+    QState *result_state = new QState(); machine.addState(result_state);
+
+    connect(welcome_state, SIGNAL(entered()), this, SLOT(initWelcomeState()));
+    welcome_state->addTransition(connect_button, SIGNAL(clicked()), processing_state);
+    connect(processing_state, SIGNAL(entered()), this, SLOT(initProcessingState()));
+    processing_state->addTransition(cancel_button, SIGNAL(clicked()), welcome_state);
+    connect(result_state, SIGNAL(entered()), this, SLOT(initResultState()));
+    result_state->addTransition(towelcome_button, SIGNAL(clicked()), welcome_state);
+    connect(processing_state, SIGNAL(entered()), this, SLOT(initWelcomeState()));
+
+    machine.setInitialState(welcome_state);
+    machine.start();
+
+}
+
+void qtDictApp::initWelcomeState(){
+    qDebug()<<"welcome state entered";
+    this->setLayout(welcome_screen);
+}
+void qtDictApp::initProcessingState(){
+    qDebug()<<"processing state entered";
+    this->setLayout(processing_screen);
+}
+void qtDictApp::initResultState(){
+    qDebug()<<"result state entered";
+    this->setLayout(result_screen);
 }
 
 qtDictApp::~qtDictApp()
--- qtdictapp.h
+++ qtdictapp.h
@@ -3,14 +3,28 @@
 
 #include <QtGui/QWidget>
 #include <QObject>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QStateMachine>
+#include <QtDebug>
 
 class qtDictApp : public QWidget
 {
     Q_OBJECT
 
+    QVBoxLayout *welcome_screen;
+    QVBoxLayout *processing_screen;
+    QVBoxLayout *result_screen;
+
 public:
     qtDictApp(QWidget *parent = 0);
     ~qtDictApp();
+
+public slots:
+    void initWelcomeState();
+    void initProcessingState();
+    void initResultState();
 };
 
 #endif // QTDICTAPP_H
