a3dd55a1ae38c0c52cd6741cdd2e54c7495ab9ad
Mihhail Sidorin <uzzermail@gmail.com>
2012-02-08 12:10:50 +0200

    license added

--- qtdictapp.cpp
+++ qtdictapp.cpp
@@ -1,7 +1,39 @@
+/**
+ * @file
+ * @author  Mihhail Sidorin <msidorin@gmail.com>
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * This is main widget of Dictionary
+ */
+
+#define WELCOME_SCREEN_ID 0
+#define CONNECT_SCREEN_ID 1
+#define QUERY_SCREEN_ID 2
+#define PROCESSING_SCREEN_ID 3
+#define RESULT_SCREEN_ID 4
 #include "qtdictapp.h"
 
 
-
+/**
+* Constructor that run the progeam
+*
+* @param parent parent element where to apply Qtwidget
+*/
 qtDictApp::qtDictApp(QWidget *parent)
     : QWidget(parent)
 {   
@@ -39,7 +71,7 @@ qtDictApp::qtDictApp(QWidget *parent)
     main_layout->addWidget(welcome_wrapper);
     main_layout->addWidget(processing_wrapper);
     main_layout->addWidget(result_wrapper);
-    main_layout->setCurrentIndex(0);
+    main_layout->setCurrentIndex(WELCOME_SCREEN_ID);
 
     this->setLayout(main_layout);
 
@@ -65,10 +97,15 @@ qtDictApp::qtDictApp(QWidget *parent)
 
 }
 
+/**
+* Initialize welcome screen when welcome state is activated
+*/
 void qtDictApp::initWelcomeState(){
     qDebug()<<"welcome state entered";
-    main_layout->setCurrentIndex(0);
+    main_layout->setCurrentIndex(WELCOME_SCREEN_ID);
 }
+
+
 void qtDictApp::initProcessingState(){
     qDebug()<<"processing state entered";
     main_layout->setCurrentIndex(1);
